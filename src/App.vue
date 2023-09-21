4b3689cd1dd172793e4ee0fcfff8518c4b0129e9
anasAbounouar <anas.2002.abounouar@gmail.com>
2023-09-21 23:10:57 +0100

    made my first pageloader

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,32 @@
 <template>
-  <Navbar />
+  <Navbar v-if="isloaded" />
+  <PageLoader v-if="!isloaded" />
   <router-view />
 </template>
 <script>
 import Navbar from "@/components/NavbarView.vue";
+import PageLoader from "@/components/PageLoaderView.vue";
 export default {
   components: {
     Navbar,
+    PageLoader,
+  },
+  data() {
+    return {
+      isloaded: false,
+    };
+  },
+  mounted() {
+    document.onreadystatechange = () => {
+      if (document.readyState == "complete") {
+        // Set isloaded to true in the App.vue component
+        this.isloaded = true;
+      }
+    };
   },
 };
 </script>
+PageLoader
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
